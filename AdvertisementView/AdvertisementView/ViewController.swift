a9bea51cbbf98e70546214e0e5c806daf8426738
MacOMNI <414294494@qq.com>
2020-11-03 14:48:43 +0800

    # app advertisementview

---
+++ AdvertisementView/AdvertisementView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdvertisementView
+//
+//  Created by Mackun on 2020/11/2.
+//  Copyright © 2020 Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
